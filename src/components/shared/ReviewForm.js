1544be955ff7ce093865cd5cf19cfd8bb2dfcce2
chrislee1996 <prepchrislee@gmail.com>
2022-04-07 16:30:25 -0400

    removed edit buttons and form for reviews

--- src/components/shared/ReviewForm.js
+++ src/components/shared/ReviewForm.js
@@ -6,45 +6,6 @@ import { addReview, removeReview } from '../../api/reviews'
 const ReviewForm = (props) => {
     
     const {handleChange, handleSubmit,review} = props
-    // const [review, setReview] = useState("")
-
-
-
-    // const handleChange = (e) => {
-    //     // e === event
-    //     e.persist()
-
-    //     setReview(prevReview => {
-    //         const name = e.target.name
-    //         let value = e.target.value
-
-    //         const updatedValue = { [name]: value }
-
-
-    //         return {...prevReview, ...updatedValue}
-    //     })
-    // }
-
-    // const clearField = () => {
-    //     setReview({note: ""})
-    // }
-
-    // const destroyReview =() => {
-    //     removeReview(user, product._id, review._id)
-    //     .then(()=> triggerRefresh())
-    //     .catch(console.error)
-    // }
-
-    // const handleSubmit = (e) => {
-    //     // e === event
-    //     e.preventDefault()
-
-    //     addReview(user, product.id, review)
-    //         .then(()=> clearField())
-    //         .then(() => triggerRefresh())
-    //         // if there is an error, we'll send an error message
-    //         .catch(console.error)
-    // }
 
     return (
         <Container className="justify-content-center">
