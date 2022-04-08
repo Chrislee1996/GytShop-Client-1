7853803354e297e68b1c26f206a57a518e5a79af
chrislee1996 <prepchrislee@gmail.com>
2022-04-07 21:25:46 -0400

    comment out checkout form

--- src/components/shared/CheckoutForm.js
+++ src/components/shared/CheckoutForm.js
@@ -1,36 +1,36 @@
-import React, { useState, Link } from 'react'
-import { Form, Container, Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+// import React, { useState, Link } from 'react'
+// import { Form, Container, Button } from 'react-bootstrap'
+// import { Link } from 'react-router-dom'
 
 
 
 
-const CheckoutForm = (props) => {
-    const {handleChange, handleSubmit, heading, order, user} = props
+// const CheckoutForm = (props) => {
+//     const {handleChange, handleSubmit, heading, order, user} = props
 
-    return (
-        <Container className="justify-content-center">
-             <h3>{heading}</h3>
-{/* 
-            <Form onSubmit={handleSubmit}>
-                <Form.Label>Full Name</Form.Label>
-                <Form.Control
-                    placeholder="Full Name"
-                    onChange={handleChange}
-                />
-                <Form.Label>Shipping Address</Form.Label>
-                <Form.Control
-                    placeholder="Shipping Address"
-                    name='shippingAddress'
-                    // value={order.shippingAddress}
-                    onChange={handleChange}
-                />
-                <Link to={`/orders/${user._id}/payment`}>
-                    <Button type='submit'> Submit </Button>
-                </Link>
-            </Form> */}
-        </Container>
-    )
-}
+//     return (
+//         <Container className="justify-content-center">
+//              <h3>{heading}</h3>
+// {/* 
+//             <Form onSubmit={handleSubmit}>
+//                 <Form.Label>Full Name</Form.Label>
+//                 <Form.Control
+//                     placeholder="Full Name"
+//                     onChange={handleChange}
+//                 />
+//                 <Form.Label>Shipping Address</Form.Label>
+//                 <Form.Control
+//                     placeholder="Shipping Address"
+//                     name='shippingAddress'
+//                     // value={order.shippingAddress}
+//                     onChange={handleChange}
+//                 />
+//                 <Link to={`/orders/${user._id}/payment`}>
+//                     <Button type='submit'> Submit </Button>
+//                 </Link>
+//             </Form> */}
+//         </Container>
+//     )
+// }
 
-export default CheckoutForm 
+// export default CheckoutForm 
